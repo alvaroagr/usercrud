b5873df4c349db90d6792ed5b82963225b994ccb
alvaroagr <alvaro.gomez.rey@gmail.com>
2020-05-23 22:05:38 -0500

    default route to vuejs

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname+'public/index.html'))
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
